f5612b0071334cb658e397485277d5795dc62519
David Urting <David.Urting@lv.vlaanderen.be>
2025-05-17 12:37:46 +0200

    poort 4211/3011 (hoofdstuk 11)

--- Client/src/app/klok.service.ts
+++ Client/src/app/klok.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, firstValueFrom } from 'rxjs';
+import { Observable, firstValueFrom, map } from 'rxjs';
 
 import { Klok } from './klok';
 
@@ -8,15 +8,18 @@ import { Klok } from './klok';
   providedIn: 'root',
 })
 export class KlokService {
-  private url = 'http://localhost:3010';
+  private url = 'http://localhost:3011';
 
   constructor(private http: HttpClient) { }
 
   getKlokken$(): Observable<Klok[]> {
-    return this.http.get<Klok[]>(`${this.url}/my-clocks`);
+    return this.http.get<any[]>(`${this.url}/my-clocks`)
+      .pipe(map((klokkenLiteralsArray: any[]) => {
+        return klokkenLiteralsArray.map(klokLiteral => new Klok(klokLiteral.name, klokLiteral.timeZone, klokLiteral.locale));
+      }));
   }
 
   getKlokken(): Promise<Klok[]> {
-    return firstValueFrom(this.http.get<Klok[]>(`${this.url}/my-clocks`));
+    return firstValueFrom(this.getKlokken$());
   }
 }
--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // De poort is niet meer hardgecodeerd: de de host van deze server kan zelf bepalen op welke poort het moet draaien.
 // Als er geen poort wordt doorgegeven dan zal de server standaard op poort 3000 draaien.
-const port = process.env.PORT ? process.env.PORT : 3010;
+const port = process.env.PORT ? process.env.PORT : 3011;
 
 // De static files middleware registreren
 app.use("/", express.static(join(__dirname, 'public','browser')));
