6c4299321086636918d092168cff016a37ec1ff4
David Urting <David.Urting@lv.vlaanderen.be>
2025-09-17 23:54:40 +0200

    appConfig was niet correct gewired.

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,21 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
 import { registerLocaleData } from '@angular/common';
 import localeNlBe from '@angular/common/locales/nl-BE';
 import localeJaJp from '@angular/common/locales/ja';
 
-import { routes } from './app/app.routes';
-import { App } from './app/app';
-import { provideHttpClient } from '@angular/common/http';
+import { App } from './app/app';import { appConfig } from './app/app.config';
 
 registerLocaleData(localeNlBe, 'nl-BE');
 registerLocaleData(localeJaJp, 'ja-JP');
 
-bootstrapApplication(App, {providers: [
-  provideZonelessChangeDetection(),
-  provideRouter(routes),
-  provideHttpClient()
-]})
+bootstrapApplication(App, appConfig)
 .catch((err) => console.error(err));
